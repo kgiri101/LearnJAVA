dd3c65bc7e281809cc55051d96adf78ae8057a2c
Kamal Giri <kgiris2o57@gmail.com>
2023-07-11 16:44:48 -0600

    third try

---
+++ src/test/test2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Why is this not working");
+
+	}
+
+}
