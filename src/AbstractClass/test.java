ff5d76727b52cd9b10781ecabe85b16cc283be71
kgiri <kgiri@CO00202006.uwslab.uwyo.edu>
2023-07-29 13:40:15 -0600

    Create the Abstract Class Module from Library's  Computer

---
+++ src/AbstractClass/test.java
@@ -0,0 +1,12 @@
+package AbstractClass;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
